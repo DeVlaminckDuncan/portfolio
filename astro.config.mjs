220bd365c1d869a8b4e13dc436d8741e92adef2c
DeVlaminckDuncan <duncandv123@gmail.com>
2026-05-09 18:28:04 +0200

    Implement SEO fundamentals

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://duncandevlaminck.be",
   output: "static",
+  integrations: [sitemap()],
 });
